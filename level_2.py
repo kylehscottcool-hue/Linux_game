e6205d037604a6d61331e0806fef74a70932e6c6
Kyle <kylehscottcool@gmail.com>
2025-11-01 18:31:55 -0500

    Changes to be committed: 	modified:   level_2.py

---
+++ level_2.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+pygame.init()
+
+#Screen dimensions
+screen_width, screen_height = 640, 480
+screen = pygame.display.set_mod((screen_width, screen_height))
+pygame.display.set_caption("Help MEEEEEEEEE")
+
+
+#Border thickness
+border_thickenss = 5
+
+
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+PURPLE = (160, 32, 240)
+
