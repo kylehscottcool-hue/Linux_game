f7e1cdfd464ae2e6a78cd81951bf3876d293ad13
Kyle <kylehscottcool@gmail.com>
2025-11-01 19:41:05 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   level_2.py
    
    level_2 was initiated with basic properties excluding
    the portal properties, as level_2 will have multiple power ups and
    moving platforms.
    
    Notes to Kyle:
      Add powerups to level_1 and level_2 and update portal properties
      on level 2

--- level_2.py
+++ level_2.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Help MEEEEEEEEE")
 
 
 #Border thickness
-border_thickenss = 5
+border_thickness= 5
 
 
 #Colors
@@ -23,3 +23,36 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 PURPLE = (160, 32, 240)
 
+
+# Platform properties (Non_moving)
+platform_width = screen_width // 3
+platform_height = 20
+platform_x = screen_width - platform_width - border_thickness
+platform_y = platform_x - 100
+platform_color = GRAY
+
+# Player properties
+player_width, player_height = 50, 50
+player_x = 100
+player_y = screen_height - player_height - border_thickness
+player_speed = 5
+player_vel_y = 0
+ongroud = False
+
+# Force variables
+gravity = 0.5
+jump_power = -20
+
+clock = pygame.time.Clock()
+running = True 
+
+while running:
+    #Code here 
+    
+    #Comment the next line out when code and game works, and can be exited
+    running = False
+    #// 
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
